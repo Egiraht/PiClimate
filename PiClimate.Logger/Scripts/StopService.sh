6a2430f66fb42281a65cd1d43449f84844c479f0
Maxim Yudin <stibiu@yandex.ru>
2020-03-26 11:21:45 +0300

    Added the license file and headers (MPL v2.0).

--- PiClimate.Logger/Scripts/StopService.sh
+++ PiClimate.Logger/Scripts/StopService.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright © 2020 Maxim Yudin <stibiu@yandex.ru>
+
 PICLIMATE_ROOT_DIR=/home/pi/PiClimate
 
 echo "Stopping the PiClimate.Logger service..."
